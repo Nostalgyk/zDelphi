d18afcf6b5a6b27ae465a16fb8b386c46a6b82a5
Gustavo Oliveira <guga.oli.1357@gmail.com>
2021-09-16 23:55:09 -0300

    Updating Insert and Delete on It's Disco! - Testing

--- 04 - DISCOS/uPrincipal.pas
+++ 04 - DISCOS/uPrincipal.pas
@@ -13,36 +13,140 @@ type
     btnAdcBanda: TButton;
     btnRemvBanda: TButton;
     altBanda: TButton;
-    ListBox1: TListBox;
+    lbxResultado: TListBox;
     btnListBanda: TButton;
     btnVerDisco: TButton;
+    btnInfo: TButton;
+    btnSair: TButton;
     procedure btnAdcBandaClick(Sender: TObject);
+    procedure btnInfoClick(Sender: TObject);
+    procedure btnRemoverClick(Sender: TObject);
+    procedure btnSairClick(Sender: TObject);
+
   private
-    { Private declarations }
+    type
+      TTipoMensagem = (Informacao, Alerta, Erro);
+
+      const
+        cNOME_SISTEMA = 'Controle de Discos';
+        cVALOR_ZERO = 0;
+        cVALOR_VAZIO = -1;
+
+      var
+        FQuantidadeRegistrosInseridos,
+        FQuantidadeRegistrosExcluidos: integer;
+
+        procedure ValidarInsercao;
+        procedure ValidarExclusao;
+        procedure ValidarLancamentoDuplicado;
+        procedure AdicionarBanda;
+        procedure RemoverBanda;
+
+        procedure ExibirMensagem(pMensagem: string; pTipoMensagem: TTipoMensagem);
   public
     { Public declarations }
+
   end;
 
 var
   frmDisco: TfrmDisco;
 
+
 implementation
 
 {$R *.dfm}
 
-uses uCadastroBanda;
+uses
+  uCadastroBanda;
+
+procedure TfrmDisco.AdicionarBanda;
+begin
+  lbxResultado.Items.Add(cboBanda.Text);
+  cboBanda.ItemIndex := -1;
+end;
 
 procedure TfrmDisco.btnAdcBandaClick(Sender: TObject);
-var
-NomeBanda: String;
   begin
+    ValidarInsercao;
+    ValidarLancamentoDuplicado;
+    AdicionarBanda;
+    Inc(FQuantidadeRegistrosInseridos);
+end;
 
-    try
-      Application.CreateForm(TfrmCadBanda, frmDisco);
-      frmCadBanda.ShowModal;
-    finally
-      FreeAndNil(frmCadBanda);
-    end;
+procedure TfrmDisco.btnInfoClick(Sender: TObject);
+begin
+  ExibirMensagem('Informações: ' + #13 + #13 +
+  'Total de Registros: ' + IntToStr(lbxResultado.Items.Count) + #13 +
+  'Total de Lançamentos: ' + IntToStr(FQuantidadeRegistrosInseridos) + #13 +
+  'Total de Exclusões: ' + IntToStr(FQuantidadeRegistrosExcluidos), Informacao);
+end;
 
+procedure TfrmDisco.btnRemoverClick(Sender: TObject);
+begin
+  ValidarExclusao;
+  RemoverBanda;
+  Inc(FQuantidadeRegistrosExcluidos);
+end;
+
+procedure TfrmDisco.btnSairClick(Sender: TObject);
+begin
+  Halt;
+end;
+
+//   try
+//      Application.CreateForm(TfrmCadBanda, frmDisco);
+//      frmCadBanda.ShowModal;
+//    finally
+//      FreeAndNil(frmCadBanda);
+//    end;
+//
+//  end;
+
+procedure TfrmDisco.ExibirMensagem(pMensagem: string;
+  pTipoMensagem: TTipoMensagem);
+begin
+  case pTipoMensagem of
+  Informacao: Application.MessageBox(PChar(pMensagem), PWideChar(cNOME_SISTEMA), MB_ICONINFORMATION);
+  Alerta: Application.MessageBox(PChar(pMensagem), PWideChar(cNOME_SISTEMA), MB_ICONWARNING);
+  Erro : Application.MessageBox(PChar(pMensagem), PWideChar(cNOME_SISTEMA), MB_ICONERROR);
   end;
+end;
+
+procedure TfrmDisco.RemoverBanda;
+begin
+  lbxResultado.Items.Delete(lbxResultado.ItemIndex);
+end;
+
+procedure TfrmDisco.ValidarExclusao;
+begin
+  if lbxResultado.Items.Count = cVALOR_ZERO then
+  begin
+    ExibirMensagem('Não há registros a serem excluídos.', Alerta);
+    Abort;
+  end;
+
+  if lbxResultado.Items.Count = cVALOR_VAZIO then
+  begin
+    ExibirMensagem('Selecione a banda a ser removida.', Alerta);
+  end;
+end;
+
+procedure TfrmDisco.ValidarInsercao;
+begin
+  ExibirMensagem('Selecione a banda a ser adicionada.', Alerta);
+  cboBanda.SetFocus;
+  Abort;
+end;
+
+procedure TfrmDisco.ValidarLancamentoDuplicado;
+var
+  i: integer;
+begin
+  for i:= cVALOR_ZERO to Pred(lbxResultado.Items.Count) do
+  begin
+    ExibirMensagem('A banda "' + cboBanda.Text + '" já foi adicionada.' + #13 +
+      'A operação será cancelada.', Alerta);
+      Abort;
+  end;
+end;
 end.
---
+++ 04 Discos 2/__recovery/uPrincipal.pas
@@ -0,0 +1,154 @@
+unit uPrincipal;
+
+interface
+
+uses
+  Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls;
+
+type
+  TfrmPrincipal = class(TForm)
+    pnlPrincipal: TPanel;
+    gbxControle: TGroupBox;
+    lblBanda: TLabel;
+    cboBanda: TComboBox;
+    btnAdicionar: TBitBtn;
+    btnRemover: TBitBtn;
+    lbxResultado: TListBox;
+    btnSair: TBitBtn;
+    btnInformacoes: TBitBtn;
+    procedure btnAdicionarClick(Sender: TObject);
+    procedure btnRemoverClick(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure btnInformacoesClick(Sender: TObject);
+    procedure btnSairClick(Sender: TObject);
+  private
+  type
+    TTipoMensagem = (Informacao, Alerta, Erro);
+
+  const
+    cNOME_SISTEMA = 'Controle de Discos';
+    cVALOR_ZERO = 0;
+    cVALOR_VAZIO = -1;
+
+  var
+    FQuantidadeRegistrosInseridos,
+    FQuantidadeRegistrosExcluidos: integer;
+
+    procedure ExibirMensagem(pMensagem: string; pTipoMensagem: TTipoMensagem);
+
+    procedure ValidarInsercao;
+    procedure ValidarExclusao;
+    procedure ValidarLancamentoDuplicado;
+    procedure AdicionarBanda;
+    procedure RemoverBanda;
+
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+uses
+  Winapi.Windows;
+
+{$R *.dfm}
+
+procedure TfrmPrincipal.AdicionarBanda;
+begin
+  lbxResultado.Items.Add(cboBanda.Text);
+  cboBanda.ItemIndex := -1;
+end;
+
+procedure TfrmPrincipal.btnAdicionarClick(Sender: TObject);
+begin
+  ValidarInsercao;
+  ValidarLancamentoDuplicado;
+  AdicionarBanda;
+  Inc(FQuantidadeRegistrosInseridos);
+end;
+
+procedure TfrmPrincipal.btnInformacoesClick(Sender: TObject);
+begin
+  ExibirMensagem('Informações: ' + #13 + #13 +
+    'Total de registros: ' + IntToStr(lbxResultado.Items.Count) + #13 +
+    'Total de lançamentos: ' + IntToStr(FQuantidadeRegistrosInseridos) + #13 +
+    'Total de exclusões: ' + IntToStr(FQuantidadeRegistrosExcluidos), Informacao);
+end;
+
+procedure TfrmPrincipal.btnRemoverClick(Sender: TObject);
+begin
+  ValidarExclusao;
+  RemoverBanda;
+  Inc(FQuantidadeRegistrosExcluidos);
+end;
+
+procedure TfrmPrincipal.btnSairClick(Sender: TObject);
+begin
+  Halt;
+end;
+
+procedure TfrmPrincipal.ExibirMensagem(pMensagem: string;
+  pTipoMensagem: TTipoMensagem);
+begin
+  case pTipoMensagem of
+    Informacao: Application.MessageBox(PChar(pMensagem), PWideChar(cNOME_SISTEMA), MB_ICONINFORMATION);
+    Alerta: Application.MessageBox(PChar(pMensagem), PWideChar(cNOME_SISTEMA), MB_ICONWARNING);
+    Erro: Application.MessageBox(PChar(pMensagem), PWideChar(cNOME_SISTEMA), MB_ICONERROR);
+  end;
+end;
+
+procedure TfrmPrincipal.FormActivate(Sender: TObject);
+begin
+  FQuantidadeRegistrosInseridos := cVALOR_ZERO;
+  FQuantidadeRegistrosExcluidos := cVALOR_ZERO;
+end;
+
+procedure TfrmPrincipal.RemoverBanda;
+begin
+  lbxResultado.Items.Delete(lbxResultado.ItemIndex);
+end;
+
+procedure TfrmPrincipal.ValidarExclusao;
+begin
+  if lbxResultado.Items.Count = cVALOR_ZERO then
+  begin
+    ExibirMensagem('Não há registros a serem excluídos.', Alerta);
+    Abort;
+  end;
+
+  if lbxResultado.ItemIndex = cVALOR_VAZIO then
+  begin
+    ExibirMensagem('Selecione a banda a ser removida.', Alerta);
+    Abort;
+  end;
+end;
+
+procedure TfrmPrincipal.ValidarInsercao;
+begin
+  if cboBanda.ItemIndex = cVALOR_VAZIO then
+  begin
+    ExibirMensagem('Selecione a banda a ser adicionada.', Alerta);
+    cboBanda.SetFocus;
+    Abort;
+  end;
+end;
+
+procedure TfrmPrincipal.ValidarLancamentoDuplicado;
+var
+  i: Integer;
+begin
+  for i := cVALOR_ZERO to Pred(lbxResultado.Items.Count) do
+    if lbxResultado.Items[i] = cboBanda.Text then
+    begin
+      ExibirMensagem('A banda "' + cboBanda.Text + '" já foi adicionada.' + #13 +
+        'A operação será cancelada.', Alerta);
+      Abort;
+    end;
+end;
+
+end.
